3a5e9d4e852768d55c32b26d70474d9a89b93555
T.Krivonos <tatyana.krivonos@binomo.com>
2020-10-01 21:23:12 +0300

    task3.2

--- js/main.js
+++ js/main.js
@@ -14,9 +14,16 @@
   const map = document.querySelector(`.map`);
   const similarAdElement = map.querySelector(`.map__pins`);
   const similarAdTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+  const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+  const mapFiltersContainer = map.querySelector(`.map__filters-container`);
 
   const getRandomNum = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
   const getRandomEl = (list) => list[getRandomNum(0, list.length - 1)];
+  const getRandomArr = (arr) => {
+    const firstNum = getRandomNum(0, arr.length - 1);
+    const lastNum = getRandomNum(firstNum, arr.length - 1);
+    return arr.splice(firstNum, lastNum);
+  };
 
   const createAds = (amount) => Array.from({length: amount}, (_, i) => ({
     author: {
@@ -31,13 +38,13 @@
       guests: getRandomEl(GUESTS),
       checkin: getRandomEl(CHECKIN_TIME),
       checkout: getRandomEl(CHECKOUT_TIME),
-      features: FEATURES.splice(getRandomNum(0, FEATURES.length - 1), getRandomNum(0, FEATURES.length - 1)),
+      features: getRandomArr(FEATURES),
       description: `description${i + 1}`,
-      photos: PHOTOS.splice(getRandomNum(0, PHOTOS.length - 1), getRandomNum(0, PHOTOS.length - 1)),
+      photos: getRandomArr(PHOTOS),
     },
     location: {
       x: getRandomNum(0, map.clientWidth),
-      y: getRandomNum(130, 630)
+      y: getRandomNum(130, 630),
     }
   }));
 
@@ -52,8 +59,46 @@
     return adElement;
   };
 
+  const renderAdPopup = (ad) => {
+    const adPopup = cardTemplate.cloneNode(true);
+
+    adPopup.querySelector(`.popup__title`).textContent = ad.offer.title;
+    adPopup.querySelector(`.popup__text--address`).textContent = ad.offer.address;
+    adPopup.querySelector(`.popup__text--price`).textContent = ad.offer.price + `₽/ночь`;
+    adPopup.querySelector(`.popup__type`).textContent = ad.offer.type;
+    adPopup.querySelector(`.popup__text--capacity`).textContent = ad.offer.rooms + ` комнаты для ` + ad.offer.guests;
+    adPopup.querySelector(`.popup__text--time`).textContent = `Заезд после ` + ad.offer.checkin + `, выезд до ` + ad.offer.checkout;
+
+    const popupFeatures = adPopup.querySelector(`.popup__features`);
+    ad.offer.features.forEach((el) => {
+      const popupFeatureItem = document.createElement(`li`);
+      popupFeatureItem.className = `popup__feature popup__feature--${el}`;
+      popupFeatures.appendChild(popupFeatureItem);
+    });
+
+    adPopup.querySelector(`.popup__description`).textContent = ad.offer.description;
+    const popupPhotos = adPopup.querySelector(`.popup__photos`);
+    ad.offer.photos.forEach((el) => {
+      const popupPhotoItem = document.createElement(`img`);
+      popupPhotoItem.className = `popup__photo`;
+      popupPhotoItem.src = el;
+      popupPhotoItem.width = 45;
+      popupPhotoItem.height = 40;
+      popupPhotoItem.alt = `Фотография жилья`;
+      popupPhotos.appendChild(popupPhotoItem);
+    });
+    adPopup.querySelector(`.popup__avatar`).src = ad.author.avatar;
+
+    return adPopup;
+  };
+
   map.classList.remove(`map--faded`);
 
-  createAds(8).forEach((ad) => fragment.appendChild(renderAd(ad)));
+  const similarAds = createAds(8);
+
+  similarAds.forEach((ad) => fragment.appendChild(renderAd(ad)));
   similarAdElement.appendChild(fragment);
+
+  fragment.appendChild(renderAdPopup(similarAds[0]));
+  map.insertBefore(fragment, mapFiltersContainer);
 })();
