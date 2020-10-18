261e21384e9dc40ba3900a040df6ee75e42ccbd3
T.Krivonos <tatyana.krivonos@binomo.com>
2020-10-18 19:14:52 +0300

    task5-2

---
+++ js/move.js
@@ -0,0 +1,40 @@
+'use strict';
+(function () {
+  const MAIN_PIN_HEIGHT_ACTIVE = 87;
+  const map = document.querySelector(`.map`);
+  const mainPin = map.querySelector(`.map__pin--main`);
+  const mapLimits = {
+    top: 130,
+    right: map.offsetWidth - (mainPin.clientWidth / 2),
+    left: -(mainPin.clientWidth / 2),
+    bottom: 630,
+  };
+
+  const relocate = (newLocation) => {
+    mainPin.style.left = newLocation.x + `px`;
+    mainPin.style.top = newLocation.y + `px`;
+    window.util.getNewMainPinAddress(newLocation.x, newLocation.y, MAIN_PIN_HEIGHT_ACTIVE);
+  };
+
+  const getNewLocation = (newMainPinX, newMainPinY) => {
+    let newLocation = {
+      x: mapLimits.left,
+      y: mapLimits.top
+    };
+    if (newMainPinX > mapLimits.right) {
+      newLocation.x = mapLimits.right;
+    } else if (newMainPinX > mapLimits.left) {
+      newLocation.x = newMainPinX;
+    }
+    if (newMainPinY > mapLimits.bottom) {
+      newLocation.y = mapLimits.bottom;
+    } else if (newMainPinY > mapLimits.top) {
+      newLocation.y = newMainPinY;
+    }
+    relocate(newLocation);
+  };
+
+  window.move = {
+    getNewLocation,
+  };
+})();
